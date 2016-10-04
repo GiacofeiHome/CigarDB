3b1f2805fd2c02f5cf90d8fdb29ebb2a1b091d67
chris <c.giacofei@gmail.com>
2016-10-03 22:14:04 -0400

    Add some cigardb models

--- project/server/models.py
+++ project/server/models.py
@@ -38,3 +38,62 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {0}>'.format(self.email)
+
+
+class Brand(db.Model):
+
+    __tablename__ = "brands"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+
+    products = db.relationship('Product', backref='brands', lazy='dynamic')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+
+class Product(db.Model):
+
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    brand = db.Column(db.Integer, db.ForeignKey('brands.id'))
+
+    inventory = db.relationship('Inventory', backref='products', lazy='dynamic')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+
+class Size(db.Model):
+
+    __tablename__ = "sizes"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+
+    inventory = db.relationship('Inventory', backref='sizes', lazy='dynamic')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+class Location(db.Model):
+
+    __tablename__ = "locations"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+
+    inventory = db.relationship('Inventory', backref='locations', lazy='dynamic')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+
+class Inventory(db.Model):
+
+    __tablename__ = "cigars"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    hash = db.Column(db.String(64))
+    product = db.Column(db.Integer, db.ForeignKey('products.id'))
+    size = db.Column(db.Integer, db.ForeignKey('sizes.id'))
+    location = db.Column(db.Integer, db.ForeignKey('locations.id'))
